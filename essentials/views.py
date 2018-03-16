f9b533d345bda77a33ceee206d39e876b731a1d6
jmushtaq <jawaid_mushtaq@yahoo.com>
2018-03-16 09:41:55 +0800

    created base essentials app

--- essentials/views.py
+++ essentials/views.py
@@ -1,6 +1,42 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.views import generic
 from django.shortcuts import render
 
-# Create your views here.
+
+class IndexView(generic.TemplateView):
+    """ TemplateView does not require a model queryset """
+    template_name = 'essentials/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+
+        context.update({
+                'days': [1, 2, 3],
+            })
+
+        return context
+
+
+class _IndexView(generic.ListView):
+    template_name = 'essentials/index.html'
+    context_object_name = 'latest_question_list'
+
+#    def get_queryset(self):
+#        """Return the last five published questions."""
+#        return Question.objects.order_by('-pub_date')[:5]
+
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+
+        context.update({
+                'days': [1, 2, 3],
+            })
+
+        return context
+
+    def get_queryset(self):
+		return None
+
+
